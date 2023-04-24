5386b22656454aa2097d77b7ec78e9a02f407da9
Isabelle Rhoads <isabellerhoads@tamu.edu>
2023-04-24 18:28:44 -0500

    add bootstrap, fixed credentials

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
 $servername = "localhost";
-$username = "mooreh2";
-$password = "duBhop-7huhpo-qavkyb";
+$username = "root";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username);
 
 // Check connection
 if($conn -> connect_error)
@@ -13,19 +12,33 @@ if($conn -> connect_error)
 die("Connection failed:" . $conn->connect_error);
 
 }
-print("Connection successful");
 
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>My First Website</title>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<h1>Hello, World!</h1>
-
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Punch</a>
+    </div>
+    <ul class="nav navbar-nav">
+        <li><a href="#">Profile</a></li>
+      <li><a href="#">Appointments</a></li>
+      <li><a href="#">Chats</a></li>
+    </ul>
+  </div>
+</nav>
 
 </body>
 </html>
