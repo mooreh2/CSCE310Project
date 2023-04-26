7d8c76da88289185cb7f1a04a5787d3d797f4081
mooreh2 <mooreh2@tamu.edu>
2023-04-26 15:45:47 -0500

    Styling changes to login and structure for inbox page

--- index.php
+++ index.php
@@ -25,20 +25,56 @@ die("Connection failed:" . $conn->connect_error);
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-<body>
-
+<body style="background-color: #eee;">
+<div>
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">Punch</a>
     </div>
     <ul class="nav navbar-nav">
-        <li><a href="#">Profile</a></li>
-      <li><a href="#">Appointments</a></li>
-      <li><a href="#">Chats</a></li>
+        <li><a href="/">Login</a></li>
+        <li><a href="/">My Profile</a></li>
+      <li><a href="/">Appointments</a></li>
+      <li><a href="/inbox.php">Inbox</a></li>
     </ul>
   </div>
 </nav>
+</div>
+<br></br><br></br>
+<div class="justify-content-center align-items-center h-500">
+<section class="vh-100 gradient-custom justify-content-center align-center">
+  <div class="container py-5 h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Please enter your username and password.</p>
+
+              <div class="form-outline form-white mb-4">
+                <input type="Username" id="typeEmailX" class="form-control form-control-lg" />
+                <label class="form-label" for="typeEmailX">Username</label>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="Password" id="typePasswordX" class="form-control form-control-lg" />
+                <label class="form-label" for="typePasswordX">Password</label>
+              </div>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</div>
 
 </body>
 </html>
