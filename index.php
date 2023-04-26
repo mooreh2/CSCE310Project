83ab876b2b2d37d641c5f47b00b4812491d9fb1b
mooreh2 <mooreh2@tamu.edu>
2023-04-26 14:25:50 -0500

    Adjusted home page, slightly better styling

--- index.php
+++ index.php
@@ -40,11 +40,10 @@ die("Connection failed:" . $conn->connect_error);
   </div>
 </nav>
 </div>
-
-<div>
-<section class="vh-100 gradient-custom">
+<br></br><br></br>
+<div class="justify-content-center align-items-center h-500">
+<section class="vh-100 gradient-custom justify-content-center align-center">
   <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
         <div class="card bg-dark text-white" style="border-radius: 1rem;">
           <div class="card-body p-5 text-center">
@@ -52,11 +51,11 @@ die("Connection failed:" . $conn->connect_error);
             <div class="mb-md-5 mt-md-4 pb-5">
 
               <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-              <p class="text-white-50 mb-5">Please enter your login and password!</p>
+              <p class="text-white-50 mb-5">Please enter your username and password.</p>
 
               <div class="form-outline form-white mb-4">
                 <input type="Username" id="typeEmailX" class="form-control form-control-lg" />
-                <label class="form-label" for="typeEmailX">Email</label>
+                <label class="form-label" for="typeEmailX">Username</label>
               </div>
 
               <div class="form-outline form-white mb-4">
