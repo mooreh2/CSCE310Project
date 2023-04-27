e046c20b80cb8f9d64885d0106761d0a92e14980
Vardaan Kola <vardaan.kola@gmail.com>
2023-04-27 14:46:43 -0500

    Added profile.php and inbox.php

--- inbox.php
+++ inbox.php
@@ -34,7 +34,7 @@ die("Connection failed:" . $conn->connect_error);
     </div>
     <ul class="nav navbar-nav">
         <li><a href="/">Login</a></li>
-        <li><a href="/">My Profile</a></li>
+        <li><a href="/profile.php">My Profile</a></li>
       <li><a href="/">Appointments</a></li>
       <li><a href="/inbox.php">Inbox</a></li>
     </ul>
