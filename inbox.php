751f4113665db031a33ca0d21e922240e68caf5d
mooreh2 <mooreh2@tamu.edu>
2023-05-02 11:15:58 -0500

    Added documentation to my files

--- inbox.php
+++ inbox.php
@@ -1,3 +1,18 @@
+<!-- 
+  File Author: Hayden Moore
+  File Description:
+    This page holds the inbox for the current user.
+    The current user will have a list displayed of all of the other users that they have either sent or received a message from.
+    Each name displayed will have a button for the user to click to enter the message room specific to them and that other user.
+-->
+
+<!-- 
+  In the PHP portion, a database connection is created, the connection is checked, 
+  and a session is started to find the currently logged in user.
+  Both users and messages are queried from our database.
+  Based on the results of these queries, an algorithm finds the users the current user has messages with.
+  Finally, once one of the buttons is clicked by the user, they will be redirected to message.php
+-->
 <?php
 
 $servername = "localhost";
@@ -26,7 +41,6 @@ $msgResult = $msgResult->fetch_all();
 $userSql = "SELECT * FROM `user`;";
 $userResult = $conn->query($userSql);
 $userResult = $userResult->fetch_all();
-
 
 // Finding the user's the current user has messages with
 $usersWithMessagesDup = [];
@@ -62,9 +76,16 @@ if(isset($_POST['submit'])) {
 
 ?>
 
+<!-- 
+  Styling for this page consists of the nav bar,
+  a header showing who is logged in,
+  and a list of all of the other users that the logged in user has messages with.
+  Within each list element, the name of the given user, and a button that can be clicked to enter that user's message room is available.
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Page setup -->
   <title>Inbox</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -72,50 +93,55 @@ if(isset($_POST['submit'])) {
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
+
 <body style="background-color: #eee;">
-<div>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Punch</a>
+  <div>
+  <!-- Nav Bar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Punch</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="/">Login</a></li>
+            <li><a href="/profile.php">My Profile</a></li>
+            <li><a href="/">Appointments</a></li>
+            <li><a href="/inbox.php">Inbox</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  <br/><br/><br/>
+
+  <div class="container d-flex justify-content-center">
+    <div class="page-header">
+      <h1 class="text-center"><?php echo $currentUser[1] .' ' .$currentUser[2] .'\'s Inbox';?></h1>
     </div>
-    <ul class="nav navbar-nav">
-        <li><a href="/">Login</a></li>
-        <li><a href="/profile.php">My Profile</a></li>
-        <li><a href="/">Appointments</a></li>
-        <li><a href="/inbox.php">Inbox</a></li>
-    </ul>
-  </div>
-</nav>
-</div>
-<br/><br/><br/>
-
-
-<div class="container d-flex justify-content-center">
-  <div class="page-header">
-    <h1 class="text-center"><?php echo $currentUser[1] .' ' .$currentUser[2] .'\'s Inbox';?></h1>
-  </div>
-		<ul class="list-group mt-5 text-white">
-      <?php
-        foreach($usersWithMessagesAsUsers as $d) {
-          echo '<li class="list-group-item d-flex justify-content-between align-content-center">
-            
-            <div class="d-flex flex-row">
-              <div class="ml-2">
-                <h3 class="mb-0">' .$d[1] .' ' .$d[2] .'</h3>
-                <div class="about">
-                  <form method="post">
-                    <input type="hidden" id="postId" name="postId" value=' .$d[1] .'>
-                    <button name="submit" class="btn btn-outline-light btn-lg px-5" type="submit">Click to view messages</button>
-                  </form>
+      <ul class="list-group mt-5 text-white">
+        <!-- 
+          On this page, we have to embed PHP within the HTML to loop through the users queried above.
+          Within the for loop, the echo command can be used to return html code.
+        -->
+        <?php
+          foreach($usersWithMessagesAsUsers as $d) {
+            echo '<li class="list-group-item d-flex justify-content-between align-content-center">
+              
+              <div class="d-flex flex-row">
+                <div class="ml-2">
+                  <h3 class="mb-0">' .$d[1] .' ' .$d[2] .'</h3>
+                  <div class="about">
+                    <form method="post">
+                      <input type="hidden" id="postId" name="postId" value=' .$d[1] .'>
+                      <button name="submit" class="btn btn-outline-light btn-lg px-5" type="submit">Click to view messages</button>
+                    </form>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>';
-        }
-      ?>
-		</ul>
-	</div>
-
+            </li>';
+          }
+        ?>
+      </ul>
+    </div>
+  </div>
 </body>
 </html>
--- message.php
+++ message.php
@@ -1,3 +1,24 @@
+<!-- 
+  File Author: Hayden Moore
+  File Description: 
+    In the actual message page with the current user and the selected other user,
+    the functionality will be similar to what an iPhon user might have in the messages app.
+    Displayed will be all messages between the two users in the database, with the sender's name prepended to each.
+    Beyond having a visual of all of these messages, a user will be able to update, delete, and send messages
+    To delete a message, the user will simply click the delete button and the message will be removed,
+    both from the page and from the database.
+    To send a message, the current user will type their desired message into the text box, click send,
+    and that new message will be shown both on the UI and will also be added to the database.
+    To update a message, the user will click the update button. 
+    After that, they will be redirected to update_message.php, which will be described in that file.
+-->
+
+<!-- 
+  In the PHP section, similarly to other files, first a connection is established with the database.
+  Then, users and messages are queried from the database,
+  and finally, based on the desired action from the user (send, update, or delete messages),
+  Specifications for how to handle this changing of data, using form HTML elements, is implemented.
+-->
 <?php
 
 $servername = "localhost";
@@ -82,12 +103,18 @@ if (isset($_POST['update'])) {
   header('Location: /update_message.php');
   exit;
 }
-
 ?>
 
+<!-- 
+  The styling of the page displays the nav bar,
+  A title of the conversation between the two users,
+  The individual messages themselves, each with an update and delete option,
+  and finally a text box to send a message.
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Page setup -->
   <title>Messages</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -95,80 +122,83 @@ if (isset($_POST['update'])) {
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
+
 <body style="background-color: #eee;">
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Punch</a>
+  <!-- Nav bar -->
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Punch</a>
+      </div>
+      <ul class="nav navbar-nav">
+          <li><a href="/">Login</a></li>
+          <li><a href="/">My Profile</a></li>
+          <li><a href="/">Appointments</a></li>
+          <li><a href="/inbox.php">Inbox</a></li>
+      </ul>
     </div>
-    <ul class="nav navbar-nav">
-        <li><a href="/">Login</a></li>
-        <li><a href="/">My Profile</a></li>
-        <li><a href="/">Appointments</a></li>
-        <li><a href="/inbox.php">Inbox</a></li>
-    </ul>
-  </div>
-</nav>
-</br></br></br></br>
+  </nav>
+  </br></br></br></br>
+
+  <!-- Beginning of actual message box section -->
+  <div class="page-content page-container" id="page-content">
+      <div class="padding">
+          <div class="row container d-flex justify-content-center">
+            <div class="col-md-6">
+              <div class="card card-bordered">
+                <div class="card-header">
+                  <h4 class="card-title"><strong><?php echo $currentUser[1] .' ' .$currentUser[2] .'\'s Chat with ' .$otherUser[1] .' ' .$otherUser[2];?></strong></h4>
+                </div>
 
 
-
-<div class="page-content page-container" id="page-content">
-    <div class="padding">
-        <div class="row container d-flex justify-content-center">
-          <div class="col-md-6">
-            <div class="card card-bordered">
-              <div class="card-header">
-                <h4 class="card-title"><strong><?php echo $currentUser[1] .' ' .$currentUser[2] .'\'s Chat with ' .$otherUser[1] .' ' .$otherUser[2];?></strong></h4>
-              </div>
-
-
-              <div class="ps-container ps-theme-default ps-active-y" id="chat-content" style="overflow-y: scroll !important; height:400px !important;">
-                <div class="media media-chat">
-                  <div class="media-body">
-                    <?php 
-                      foreach($msgs as $msg) {
-                        foreach($users as $user) {
-                          if($user[0] == $msg[1]) {
-                            $currentSenderFName = $user[1];
-                            $currentSenderLName = $user[2];
-                            break;
+                <div class="ps-container ps-theme-default ps-active-y" id="chat-content" style="overflow-y: scroll !important; height:400px !important;">
+                  <div class="media media-chat">
+                    <div class="media-body">
+                      <!-- A PHP portion is needed again to iterate and display each message -->
+                      <?php 
+                        foreach($msgs as $msg) {
+                          foreach($users as $user) {
+                            if($user[0] == $msg[1]) {
+                              $currentSenderFName = $user[1];
+                              $currentSenderLName = $user[2];
+                              break;
+                            }
                           }
+                          echo '<p><u>' .$currentSenderFName .' ' .$currentSenderLName .':</u> ' .$msg[3] .'
+                              <span class="publisher bt-1">
+                                <form method="post">
+                                  <input type="hidden" name="msgToBeUpdated" value=' . $msg[0] .'>
+                                  <button name="update" class="publisher-input">Update</button>
+                                </form>
+                                <form method="post">
+                                  <input type="hidden" name="msgToBeDeleted" value=' . $msg[0] . '>
+                                  <button name="delete" class="publisher-input">Delete</button>
+                                </form>
+                              </span></p>
+                            </form>
+                            '
+                          ;
                         }
-                        echo '<p><u>' .$currentSenderFName .' ' .$currentSenderLName .':</u> ' .$msg[3] .'
-                            <span class="publisher bt-1">
-                              <form method="post">
-                                <input type="hidden" name="msgToBeUpdated" value=' . $msg[0] .'>
-                                <button name="update" class="publisher-input">Update</button>
-                              </form>
-                              <form method="post">
-                                <input type="hidden" name="msgToBeDeleted" value=' . $msg[0] . '>
-                                <button name="delete" class="publisher-input">Delete</button>
-                              </form>
-                            </span></p>
-                          </form>
-                          '
-                        ;
-                      }
-                    ?>
+                      ?>
+                    </div>
                   </div>
-                </div>
-
-              <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;"><div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; height: 0px; right: 2px;"><div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 2px;"></div></div></div>
-              
-              <form method="post">
-                <div class="publisher bt-1 border-light">
-                  <input name="addedContent"class="publisher-input" type="text" placeholder="Write something">
-                  <button name="send" class="publisher-input" type="submit">Send</button>              
-                </div>
-              </form>
-             </div>
+                
+                <!-- Scroll bar -->
+                <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;"><div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; height: 0px; right: 2px;"><div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 2px;"></div></div></div>
+                
+                <!-- Text box to send a new message -->
+                <form method="post">
+                  <div class="publisher bt-1 border-light">
+                    <input name="addedContent"class="publisher-input" type="text" placeholder="Write something">
+                    <button name="send" class="publisher-input" type="submit">Send</button>              
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
-          </div>
-          </div>
-          </div>
-        
-        
-  </body>
+        </div>
+      </div>
+          
+          
+</body>
 </html>
